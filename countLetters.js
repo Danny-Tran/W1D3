e4bc8c510e0b04931a7bd5c08191a341b6530fcc
Danny-Tran <truong_tran@live.com>
2019-05-01 14:48:11 +0000

    completed

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,15 @@
 // var output = process.argv[2]
 function countLetter(str) {
-  var splitString = str.split("")
-console.log(splitString)
+  var obj = {}
+  var splitString = str.replace(/\s/g, '')
+
+for (var i = 0; i < splitString.length; i++) {
+  if (!obj[splitString[i]]) {
+  obj[splitString[i]] = 1
+  } else {
+    obj[splitString[i]] ++
+  }
+}return obj
 }
 
 console.log(countLetter("lighthouse in the house"))
